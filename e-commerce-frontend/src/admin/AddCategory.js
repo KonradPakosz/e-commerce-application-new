310a6f311d01ebd20afbb6dda07624a6f4861fae
KonradPakosz <42554245+KonradPakosz@users.noreply.github.com>
2020-04-24 23:58:38 +0200

    add category finished

--- e-commerce-frontend/src/admin/AddCategory.js
+++ e-commerce-frontend/src/admin/AddCategory.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
+import {createCategory} from './apiAdmin';
 
 
 
@@ -23,6 +24,15 @@ const AddCategory = () => {
         setError('')
         setSuccess(false)
         // make requst to api to ceate a category
+        createCategory(user._id, token, {name})
+        .then(data => {
+            if(data.error) {
+                setError(true);
+            } else {
+                setError("");
+                setSuccess(true);
+            }
+        }) 
     }
 
     const newCategoryForm = () => (
@@ -34,18 +44,44 @@ const AddCategory = () => {
                     className="form-control"
                     onChange={handleChange}
                     value={name}
-                    autoFocus />
+                    autoFocus 
+                    required/>
             </div>
             <button className="btn btn-outline-primary">Create Category</button>
         </form>
-    )
+    );
 
+    const showSuccess = () => {
+        if(success) {
+            return <h3 className="text-success">{name} is created</h3>
+        }
+    }
+
+    const showError = () => {
+        if(error) {
+            return <h3 className="text-danger">{name} already exists!</h3>
+        }
+    }
+
+    const goBack = () => (
+        <div className="mt-5">
+            <Link to="/admin/dashboard" className="text-warning">
+                Back to Dashboard
+            </Link>
+        </div>
+    )
+ 
     return (
         <Layout
             title="Add a new category"
-            description={`Hello ${name}!, add a new category here`}>
+            description={`Hello ${user.name}! Add a new category here`}>
             <div class="row">
-                <div className="col-md-8 offset-md-2">{newCategoryForm()}</div>
+                <div className="col-md-8 offset-md-2">
+                    {newCategoryForm()}
+                    {showSuccess()}
+                    {showError()}
+                    {goBack()}
+                </div>
             </div>
         </Layout>
     )
--- e-commerce/models/category.js
+++ e-commerce/models/category.js
@@ -6,7 +6,8 @@ const categorySchema = new mongoose.Schema(
             type: String,
             trim: true,
             required: true,
-            maxlength: 35
+            maxlength: 35,
+            unique: true
         },
     }, 
     {timestamps : true}
