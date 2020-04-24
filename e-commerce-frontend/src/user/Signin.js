5afbd3a2520e8a47cd0a276069c3deb372e646b3
KonradPakosz <42554245+KonradPakosz@users.noreply.github.com>
2020-04-24 19:46:06 +0200

    Dashboard added

--- e-commerce-frontend/src/user/Signin.js
+++ e-commerce-frontend/src/user/Signin.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import Layout from '../core/layout';
-import {signin, authenticate} from '../auth';
+import {signin, authenticate, isAuthenticated} from '../auth';
 
 const Signin = () => {
     const [values, setValues] = React.useState({
@@ -13,6 +13,7 @@ const Signin = () => {
     });
 
     const { email, password, loading, error, redirectToReferrer} = values;
+    const{ user } = isAuthenticated()
 
     const handleChange = name => event => {
         setValues({...values, error: false, [name]: event.target.value});
@@ -68,7 +69,11 @@ const Signin = () => {
 
     const redirectUser = () => {
         if(redirectToReferrer) {
-            return <Redirect to="/"></Redirect>;
+            if(user && user.role === 1) {
+                return <Redirect to='/admin/dashboard' />;
+            } else {
+                return <Redirect to='/user/dashboard' />;
+            }
         }
     }
 
