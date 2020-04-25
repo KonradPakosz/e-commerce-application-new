01642cfd6c80ae5f721f5ef15430177539a78bdb
KonradPakosz <42554245+KonradPakosz@users.noreply.github.com>
2020-04-25 14:02:14 +0200

    home update

--- e-commerce-frontend/src/core/Home.js
+++ e-commerce-frontend/src/core/Home.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import Layout from './Layout'
 import {getProducts} from './apiCore'
+import Card from './Card'
 
 const Home = () => {
     const [productsBySold, setProductsBySold] = useState([]);
@@ -33,10 +34,19 @@ const Home = () => {
     }, [])
 
     return(
-        <Layout title="Home Page" description="Node React E-commerce App">
-        {JSON.stringify(productsByArrival)};
-        <hr/>
-        {JSON.stringify(productsBySold)}
+        <Layout title="Home Page" description="Node React E-commerce App" className="container-fluid">
+            <h2 className="mb-4">New Arrivals</h2>
+            <div className="row">
+                {productsByArrival.map((product, i) => (
+                    <Card key={i} product={product} />
+                ))}
+            </div>
+            <h2 className="mb-4">Best Sellers</h2>
+            <div className="row">
+                {productsBySold.map((product, i) => (
+                    <Card key={i} product={product} />
+                ))}
+            </div>
         </Layout>
     )
 };
