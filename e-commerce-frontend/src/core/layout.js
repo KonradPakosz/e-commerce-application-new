d4b184f4f5f56997ca5250bbfae5e99be9f14d39
KonradPakosz <42554245+KonradPakosz@users.noreply.github.com>
2020-04-25 14:48:00 +0200

    added some css

--- e-commerce-frontend/src/core/layout.js
+++ e-commerce-frontend/src/core/layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Menu from './Menu';
+import "../styles.css";
 
 const Layout = ({title = 'Title', description = 'Description', className, children}) => (
     <div>
         <Menu />
         <div className="jumbotron">
-            <h2>{title}</h2>
+            <h1>{title}</h1>
             <p className="lead">{description}</p>
         </div>
         <div className={className}>{children}</div>
