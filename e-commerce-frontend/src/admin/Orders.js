8212691b8ec4d107b1b2fcda977e69cb677977d6
KonradPakosz <42554245+KonradPakosz@users.noreply.github.com>
2020-04-26 15:11:09 +0200

    orders fully working/status updated in db

--- e-commerce-frontend/src/admin/Orders.js
+++ e-commerce-frontend/src/admin/Orders.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
-import { listOrders } from './apiAdmin';
+import { listOrders, getStatusValues, updateOrderStatus } from './apiAdmin';
 import moment from "moment";
 
 const Orders = () => {
     const [orders, setOrders] = useState([])
+    const [statusValues, setStatusValues] = useState([])
 
     const {user, token} = isAuthenticated()
 
@@ -20,8 +21,19 @@ const Orders = () => {
         })
     }
 
+    const loadStatusValues = () => {
+        getStatusValues(user._id, token).then(data => {
+            if(data.error) {
+                console.log(data.error)
+            } else {
+                setStatusValues(data)
+            }
+        })
+    }
+
     useEffect(() => {
         loadOrders()
+        loadStatusValues()
     }, [])
 
     const showOrdersLength = () => {
@@ -50,6 +62,33 @@ const Orders = () => {
         </div>
     );
 
+     const handleStatusChange = (e, orderId) => {
+        updateOrderStatus(user._id, token, orderId, e.target.value).then(data => {
+            if(data.error) {
+                console.log("update status failed")
+            } else {
+                loadOrders()
+            }
+        })
+    }
+
+    const showStatus = o => (
+        <div className="form-group">
+            <h3 className="mark mb-4">Status: {o.status}</h3>
+            <select 
+                className="form-control" 
+                onChange={(e) => handleStatusChange(e, o._id)}>
+                    <option>Update Status</option>
+                    {statusValues.map((status, i) =>(
+                        <option key={i} value={status}>
+                            {status}
+                        </option>
+                    ))}
+            </select>
+        </div>
+
+    )
+
     return (
         <Layout
             title="Orders"
@@ -65,7 +104,7 @@ const Orders = () => {
                                 </h2>
                                 <ul className="list-group mb-2">
                                     <li className="list-group-item">
-                                        {o.status}
+                                        {showStatus(o)}
                                     </li>
                                     <li className="list-group-item">
                                         Transacion ID: {o.transaction_id}
